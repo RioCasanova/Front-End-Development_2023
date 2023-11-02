969ee811edd85885802bbd01dd957c4f27b44fc4
Rio Casanova <riocasanova@gmail.com>
2023-11-01 22:42:47 -0600

    adding delete icon

--- react-rest-reviews-app-START/react-rest-reviews-app-START/reviews-rest-app/components/AdaptationReviewCard.js
+++ react-rest-reviews-app-START/react-rest-reviews-app-START/reviews-rest-app/components/AdaptationReviewCard.js
@@ -2,6 +2,8 @@ import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 import Avatar from "@mui/material/Avatar";
 
 export default function AdaptationReviewCard(props) {
@@ -12,6 +14,11 @@ export default function AdaptationReviewCard(props) {
           <Avatar sx={{ bgcolor: "blue" }} aria-label="recipe">
             {props.rating}
           </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <DeleteIcon />
+          </IconButton>
         }
         title={
           <Typography variant="body2" color="text.secondary">
