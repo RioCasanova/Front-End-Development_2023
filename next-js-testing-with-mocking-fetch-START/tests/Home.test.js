eab9f022d65751dd7b840a7cdee6b60f93e213e4
Rio Casanova <riocasanova@gmail.com>
2023-12-04 14:25:04 -0700

    testing code and examples

--- next-js-testing-with-mocking-fetch-START/tests/Home.test.js
+++ next-js-testing-with-mocking-fetch-START/tests/Home.test.js
@@ -3,7 +3,13 @@
 // throwing an error
 import "isomorphic-fetch"; // needed for no "fetch is not defined errors
 
+// importing the testing library
+// render component
+// select items on screen
+// act allows to make changes to the state
 import { render, screen, act } from "@testing-library/react";
+
+// allows us to have special methods that can be used with testing
 import "@testing-library/jest-dom";
 
 // import mock server to serve responses to our tests
@@ -12,6 +18,8 @@ import { setupServer } from "msw/node"; // we'll set up a "mocked" server
 
 // this is the path of the api that we want to put in our first argument
 import { BASE_URL } from "../utils/api/base.js"; // we'll need this for our "mocked" server
+
+// importing the actual component
 import Home from "../pages/index.js";
 
 // server setup
@@ -41,4 +49,59 @@ beforeAll(() => {
 // close the server after all the tests
 afterAll(() => {
   server.close();
+});
+
+// Tests
+
+describe("Test Home component", () => {
+  // Load a quote
+  it("loads a quote", async () => {
+    // wait for the component to load
+    await act(() => {
+      render(<Home />);
+    });
+    // get the quote / author elements
+    const quoteElement = screen.getByTestId("quote");
+    const authorElement = screen.getByTestId("author");
+
+    // check that they contain what is expected
+    expect(quoteElement).toHaveTextContent(QUOTE);
+    expect(authorElement).toHaveTextContent(AUTHOR);
+  });
+
+  const NEWQUOTE = "Here is a quote";
+  const NEWAUTHOR = "your mom";
+
+  // When we press the button a new quote is loaded
+  it("When we press the button a new quote is loaded", async () => {
+    // wait for the component to load
+    await act(() => {
+      render(<Home />); // our component has the button
+    });
+
+    // maker server use new mock request
+    server.use(
+      http.get(`${BASE_URL}/random`, (res, req, ctx) => {
+        // below this is going to be the "mocked" response
+        return HttpResponse.json({
+          content: NEWQUOTE,
+          author: NEWAUTHOR,
+        });
+      })
+    );
+    // get the button, author, quote element
+    const buttonElement = screen.getByTestId("new-quote-button");
+
+    // click button
+    await act(() => {
+      buttonElement.click();
+    });
+
+    const quoteElement = screen.getByTestId("quote");
+    const authorElement = screen.getByTestId("author");
+
+    // check that they contain what is expected
+    expect(quoteElement).toHaveTextContent(NEWQUOTE);
+    expect(authorElement).toHaveTextContent(NEWAUTHOR);
+  });
 });
